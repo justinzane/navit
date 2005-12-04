870c5c8513eb188dc0f497b0a12172e03fff657c
(no author) <(no author)@ffa7fe5e-494d-0410-b361-a75ebd5db220>
2005-12-04 10:38:47 +0000

    This commit was manufactured by cvs2svn to create branch 'improved_destination'.
    git-svn-id: svn://svn.code.sf.net/p/navit/code@16 ffa7fe5e-494d-0410-b361-a75ebd5db220

---
+++ branches/improved_destination/navit/src/speech.c
@@ -0,0 +1,79 @@
+/* speechd simple client program
+ * CVS revision: $Id: speech.c,v 1.3 2005-12-02 13:26:31 martin-s Exp $
+ * Author: Tomas Cerha <cerha@brailcom.cz> */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <stdarg.h>
+#include "config.h"
+#ifdef HAVE_LIBSPEECHD
+#include <libspeechd.h>
+#endif
+#include "speech.h"
+
+struct speech {
+	int sockfd;
+};
+
+struct speech *
+speech_new(void) {
+#ifdef HAVE_LIBSPEECHD
+	struct speech *this;
+	int sockfd;
+
+#ifdef SPD_FATAL
+	sockfd = spd_open("map","main",NULL);
+#else
+	sockfd = spd_init("map","main");
+#endif
+	if (sockfd == 0) 
+		return NULL;
+	this=g_new(struct speech,1);
+	if (this) {
+		this->sockfd=sockfd;
+	}
+	return this;
+#else
+	return NULL;
+#endif
+}
+
+int 
+speech_say(struct speech *this, char *text) {
+#ifdef HAVE_LIBSPEECHD
+	int err;
+
+	err = spd_sayf(this->sockfd, 2, text);
+	if (err != 1)
+		return 1;
+#endif
+	return 0;
+}
+
+int
+speech_sayf(struct speech *this, char *format, ...) {
+#ifdef HAVE_LIBSPEECHD
+	char buffer[8192];
+	va_list ap;
+	va_start(ap,format);
+	vsnprintf(buffer, 8192, format, ap);
+	return speech_say(this, buffer);
+#else
+	return 0;
+#endif
+}
+
+int 
+speech_destroy(struct speech *this) {
+#ifdef HAVE_LIBSPEECHD
+	spd_close(this->sockfd);
+	g_free(this);
+#endif
+	return 0;
+}
